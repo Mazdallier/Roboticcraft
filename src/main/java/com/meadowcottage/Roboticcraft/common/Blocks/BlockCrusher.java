d5a1760e08532fb5a0b6ce966e094179d42f4acb
Arona Jones <warlordjones@gmail.com>
2015-01-25 14:58:54 +0000

    Base container block

--- src/main/java/com/meadowcottage/Roboticcraft/common/Blocks/BlockCrusher.java
+++ src/main/java/com/meadowcottage/Roboticcraft/common/Blocks/BlockCrusher.java
@@ -1,7 +1,5 @@
 package com.meadowcottage.Roboticcraft.common.Blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
@@ -10,7 +8,7 @@ import com.meadowcottage.Roboticcraft.common.Reference.Names;
 import com.meadowcottage.Roboticcraft.common.Reference.Textures;
 import com.meadowcottage.Roboticcraft.common.tile.TileEntityCrusher;
 
-public class BlockCrusher extends BlockRoboticcraft implements ITileEntityProvider
+public class BlockCrusher extends BlockRoboticcraftContainer
 {
 
 	public BlockCrusher(Material p_i45394_1_)
@@ -19,7 +17,6 @@ public class BlockCrusher extends BlockRoboticcraft implements ITileEntityProvid
 		this.setBlockName(Names.Blocks.Crusher);
 		this.setBlockTextureName(Textures.Blocks.CrusherBlock);
 		this.setStepSound(soundTypeMetal);
-		this.isBlockContainer = true;
 	}
 
 	@Override
@@ -27,29 +24,4 @@ public class BlockCrusher extends BlockRoboticcraft implements ITileEntityProvid
 	{
 		return new TileEntityCrusher();
 	}
-
-	/**
-	 * Called whenever the block is added into the world. Args: world, x, y, z
-	 */
-	@Override
-	public void onBlockAdded(World p_149726_1_, int p_149726_2_, int p_149726_3_, int p_149726_4_)
-	{
-		super.onBlockAdded(p_149726_1_, p_149726_2_, p_149726_3_, p_149726_4_);
-	}
-
-	@Override
-	public void breakBlock(World p_149749_1_, int p_149749_2_, int p_149749_3_, int p_149749_4_, Block p_149749_5_, int p_149749_6_)
-	{
-		super.breakBlock(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_, p_149749_6_);
-		p_149749_1_.removeTileEntity(p_149749_2_, p_149749_3_, p_149749_4_);
-	}
-
-	@Override
-	public boolean onBlockEventReceived(World p_149696_1_, int p_149696_2_, int p_149696_3_, int p_149696_4_, int p_149696_5_, int p_149696_6_)
-	{
-		super.onBlockEventReceived(p_149696_1_, p_149696_2_, p_149696_3_, p_149696_4_, p_149696_5_, p_149696_6_);
-		TileEntity tileentity = p_149696_1_.getTileEntity(p_149696_2_, p_149696_3_, p_149696_4_);
-		return tileentity != null ? tileentity.receiveClientEvent(p_149696_5_, p_149696_6_) : false;
-	}
-
 }
