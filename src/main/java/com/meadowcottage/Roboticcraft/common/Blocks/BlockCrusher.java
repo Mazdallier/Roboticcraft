85df2a51cb311ff1896b3c584f2f9bc23dd6700f
Arona Jones <warlordjones@gmail.com>
2015-01-25 15:00:19 +0000

    Gui opening code

--- src/main/java/com/meadowcottage/Roboticcraft/common/Blocks/BlockCrusher.java
+++ src/main/java/com/meadowcottage/Roboticcraft/common/Blocks/BlockCrusher.java
@@ -1,9 +1,12 @@
 package com.meadowcottage.Roboticcraft.common.Blocks;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
+import com.meadowcottage.Roboticcraft.client.GuiIDs;
+import com.meadowcottage.Roboticcraft.common.Roboticcraft;
 import com.meadowcottage.Roboticcraft.common.Reference.Names;
 import com.meadowcottage.Roboticcraft.common.Reference.Textures;
 import com.meadowcottage.Roboticcraft.common.tile.TileEntityCrusher;
@@ -23,5 +26,22 @@ public class BlockCrusher extends BlockRoboticcraftContainer
 	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_)
 	{
 		return new TileEntityCrusher();
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, int par2, int par3, int par4, EntityPlayer player, int par6, float par7, float par8, float par9)
+	{
+		if (world.isRemote)
+			return true;
+		else
+		{
+			TileEntityCrusher tile = (TileEntityCrusher) world.getTileEntity(par2, par3, par4);
+
+			if ((tile == null) || player.isSneaking())
+				return false;
+
+			player.openGui(Roboticcraft.instance, GuiIDs.CRUSHER, world, par2, par3, par4);
+			return true;
+		}
 	}
 }
